ff0d71ad5cb03bfe8d85521f37081d627a809aeb
poojan Shah <poojan@poojanshah.com>
2017-07-04 10:56:52 +0100

    welcome intent

--- app/src/main/java/com/example/shahp/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/shahp/myfirstapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.shahp.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     public final String TAG="lifecycle";
@@ -12,8 +12,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /**
+         * Asssign the design xml file to your coding file
+         */
         Log.i(TAG,"  On Create");
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the next button */
+    public void nextActivity(View view){
+        //create intent
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        Log.i(TAG,"  nextActivity");
+        //start activity
+        startActivity(intent);
     }
 
 
