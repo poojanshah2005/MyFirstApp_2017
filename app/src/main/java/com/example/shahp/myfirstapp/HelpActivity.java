fa0535e778611562d9baa3be7accc1d4dd5bc07b
poojan Shah <poojan@poojanshah.com>
2017-07-04 12:17:24 +0100

    implicitWebsite

--- app/src/main/java/com/example/shahp/myfirstapp/HelpActivity.java
+++ app/src/main/java/com/example/shahp/myfirstapp/HelpActivity.java
@@ -1,26 +1,20 @@
 package com.example.shahp.myfirstapp;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
-public class WelcomeActivity extends AppCompatActivity {
-    Button btnGoto;
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_help);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        initializeUI();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,23 +25,5 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
     }
-
-    public void initializeUI(){
-        //convert xml object to java objects.
-
-        btnGoto = (Button) findViewById(R.id.button);
-    }
-
-    /** Called when the user taps the next button */
-    public void nextScreen(View view){
-        /**
-         * create explicit intent, things coded by yourself, calls for within your package.
-         */
-        Intent intent = new Intent(this, HelpActivity.class);
-        //start activity
-        startActivity(intent);
-    }
-
-
 
 }
--- app/src/main/java/com/example/shahp/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/shahp/myfirstapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.shahp.myfirstapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public final String TAG="lifecycle";
+    Button btnGoto;
+    Button btnGotoWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,34 @@ public class MainActivity extends AppCompatActivity {
          */
         Log.i(TAG,"  On Create");
         setContentView(R.layout.activity_main);
+        initializeUI();
+    }
+
+    public void initializeUI(){
+        //convert xml object to java objects.
+
+        btnGoto = (Button) findViewById(R.id.button);
+        btnGotoWebsite = (Button) findViewById(R.id.buttonWebsite);
+
     }
 
     /** Called when the user taps the next button */
-    public void nextActivity(View view){
+    public void nextScreen(View view){
         //create intent
-        Intent intent = new Intent(this, WelcomeActivity.class);
+        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
         Log.i(TAG,"  nextActivity");
+        //start activity
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the next button */
+    public void implicitWebsite(View view){
+        /**
+         * create Implicit
+         * Defined by action
+         * provide the data
+         */
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         //start activity
         startActivity(intent);
     }
